47e82bd766a919419d5c956ebe590793e1cd3e08
Michael Rider <mikepdxrider@gmail.com>
2021-11-04 16:32:25 -0700

    adds failing Order.update test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,29 +56,32 @@ describe('03_separation-of-concerns-demo routes', () => {
 
   it('Returns an Order', async() => {
     // declare a variable to store a new order object.
-    const firstNewObj = {
+    const newObj = {
       quantity: 110
     };
 
     // declare a variable to store a Order.insert(newOrderObj) call.
-    const insertResponse = await Order.insert(firstNewObj.quantity);
+    const insertResponse = await Order.insert(newObj.quantity);
     // declare a variable to store a Order.getById(insertVar.id) call.
     const getByIdResponse = await Order.getById(insertResponse.id);
     // expect the get response to be an object matching the new order object.
-    expect(getByIdResponse).toEqual({ ...firstNewObj, id: expect.any(String) });
+    expect(getByIdResponse).toEqual({ ...newObj, id: expect.any(String) });
   });
 
-  // it('Returns the updated Order', () => {
-  //   // declare a variable to store a new order object.
-
-  //   // declare a variable to store a Order.insert(newOrderObj) call.
-
-  //   // declare a variable to store a Order.update(insertVar.id, newQuantity) call.
-
-  //   // declare a variable to store a Order.getById(insertVar.id) call.
-
-  //   // expect the get response quantity to match the newQuantity.
-  // });
+  it('Returns the updated Order', async () => {
+    // declare a variable to store a new order object.
+    const newObj = {
+      quantity: 110
+    };
+    // declare a variable to store a Order.insert(newOrderObj) call.
+    const insertResponse = await Order.insert(newObj.quantity);
+    // declare a variable to store a Order.update(insertVar.id, newQuantity) call.
+    await Order.update(insertResponse.id, 120);
+    // declare a variable to store a Order.getById(insertVar.id) call.
+    const getByIdResponse = await Order.getById(insertResponse);
+    // expect the get response quantity to match the newQuantity.
+    expect(getByIdResponse).toEqual({ id: insertResponse.id, quantity: 120 });
+  });
 
   // it('Returns the deleted Order', () => {
   //   // declare a variable to store a new order object.
